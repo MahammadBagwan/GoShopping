660be7d0e184ae92883ed0ad219c5e99ce1979e4
MahammadBagwan <106239734+MahammadBagwan@users.noreply.github.com>
2022-09-08 10:35:05 +0530

    Renamed GSNormal.java file to GSNormalAcc.java

---
+++ src/com/cg/application/GSNormalAcc.java
@@ -0,0 +1,25 @@
+package com.cg.application;
+
+import com.cg.framework.NormalAcc;
+
+public class GSNormalAcc extends NormalAcc {
+
+    public GSNormalAcc(int accNo, String accNm, float charges, float deliveryCharges) {
+        super(accNo, accNm, charges, deliveryCharges);
+    }
+
+    @Override
+    public void bookProduct(float p) {
+
+    }
+
+    @Override
+    public void items(float p) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Normal Account";
+    }
+}
